00f38c58c873ae7b3e7786f9172a863501672449
Rémi Casado <remi.casado@univ-lyon1.fr>
2018-03-06 15:41:13 +0100

    Init

---
+++ comparison_classification.py
@@ -0,0 +1,119 @@
+"""Hazard Zone."""
+
+from fee.models import simple_CNN
+from fee.data import ComparisonClassifGen as CCG
+from fee.classification import Expression as Exp
+from fee.data import FER2013Dataset
+from keras import optimizers
+import sys
+
+# Limit cores usage
+from keras import backend as K
+K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_‌​parallelism_threads=9,
+                                                   inter_op_parallelism_threads=9)))
+
+# - Global Vars ---------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+if len(sys.argv) != 3:
+    print("Gib FER2013 path and created model path pls.")
+    exit()
+
+fer2013_path = sys.argv[1]
+model_path = sys.argv[2]
+
+INPUT_SHAPE = (48, 48, 1)
+PIC_PER_ROUND = 50
+OTHER_PER_ROUND = 10
+
+# - Datasets ------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+print(' LOADING CSV DATAS                                            ')
+DATAS = FER2013Dataset()
+DATAS.load_csv(fer2013_path)
+print(' -----                                                   DONE ')
+
+DATAS.shuffle()
+
+# - Model -------------------------------------------------------------
+# ---------------------------------------------------------------------
+
+expressions = [Exp.HAPPINESS, Exp.ANGER, Exp.SURPRISE,
+               Exp.SADNESS, Exp.FEAR, Exp.NEUTRAL]
+
+model = simple_CNN((48, 48, 1))  # A virer
+opt = optimizers.RMSprop(lr=0.001)  # A virer
+model.compile(opt, loss='binary_crossentropy',  # A virer
+              metrics=['accuracy'])  # A virer
+# for idexp1, exp1 in enumerate(expressions):
+#     exps = [exp1]
+
+max_pic = DATAS.get_data_length(Exp.HAPPINESS)
+max_batch = int(max_pic / PIC_PER_ROUND)
+
+for counter in range(0, max_batch):  # A virer
+    print(" ROUND : "+str(counter))
+    exps = [Exp.HAPPINESS]
+    for idexp2, exp2 in enumerate(expressions):
+        # if exp1 is not exp2:
+        if exp2 is not Exp.HAPPINESS:  # A virer
+            exps.append(exp2)
+    print(' CREATING MODEL                                           ')
+    # model = simple_CNN((48, 48, 1))
+    # opt = optimizers.RMSprop(lr=0.001)
+    # model.compile(opt, loss='binary_crossentropy',
+    #               metrics=['accuracy'])
+    print(' -----                                               DONE ')
+
+    # training_pics = DATAS.get_pictures(exps,
+    #                                    part_size=0.002,
+    #                                    part_index=counter)
+    training_pics = DATAS.get_n_pictures([Exp.HAPPINESS],
+                                         count=PIC_PER_ROUND,
+                                         start=counter)
+
+    training_pics += DATAS.get_n_pictures([Exp.ANGER, Exp.SURPRISE,
+                                           Exp.SADNESS, Exp.FEAR, Exp.NEUTRAL],
+                                          count=OTHER_PER_ROUND,
+                                          start=counter)
+
+    XT1, XT2, YT = CCG.generate_training_set(training_pics, Exp.HAPPINESS)
+    model.fit([XT1, XT2], YT, batch_size=64, epochs=1, verbose=1)
+fpath = model_path
+fpath += "HAPPINESS_TEST_MODEL.hdf5"
+model.save(fpath)
+
+
+
+
+# posV = 0
+# posT = 0
+# for i in range(0, 5):
+#     for j in range(0, 10):
+#         index = j+i*j
+#         print(' CREATING DATASETS                                            ')
+#         training_pics, posT = DATAS.get_pictures([Exp.HAPPINESS, Exp.ANGER],
+#                                                 part_size=0.02,
+#                                                 start=posT)
+#         XT1, XT2, YT = CCG.generate_training_set(training_pics, Exp.HAPPINESS)
+#         validate_pics, posV = DATAS.get_pictures([Exp.HAPPINESS, Exp.ANGER],
+#                                                 target="validation",
+#                                                 part_size=0.02,
+#                                                 start=posV)
+#         XV1, XV2, YV = CCG.generate_training_set(validate_pics, Exp.HAPPINESS)
+#         print(' -----                                                   DONE ')
+#
+#         # TODO :Add right after "YT,""
+#         # ----  validation_data=([XV1, XV2], YV),
+#
+#         model.fit([XT1, XT2], YT, batch_size=64, epochs=1, verbose=1)
+#     fpath = "./Results/comparison_classification/models/HAPPINESS_ANGER"+str(i)+".hdf5"
+#     model.save(fpath)
+#
+# validate_pics, posV = DATAS.get_pictures([Exp.SADNESS, Exp.ANGER],
+#                                         target="validation", part_size=0.2)
+# XV1, XV2, YV = CCG.generate_training_set(validate_pics, Exp.SADNESS)
+# model = load_model('/home/remi/Documents/6eSens/Works/Results/comparison_classification/models/HAPPINESS_ANGER4.hdf5')
+# scores = model.evaluate([XV1, XV2], YV)
+# print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
---
+++ comparison_evaluation.py
@@ -0,0 +1,110 @@
+"""Hazard Zone."""
+
+from fee.classification import Expression as Exp
+from fee.data import FER2013Dataset
+from keras.models import load_model
+import numpy
+import sys
+
+# - Global Vars ---------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+if len(sys.argv) != 3:
+    print("Gib FER2013 path and created model path pls.")
+    exit()
+
+fer2013_path = sys.argv[1]
+model_path = sys.argv[2]
+
+INPUT_SHAPE = (48, 48, 1)
+EXPRESSIONS_INDEX = {
+        Exp.ANGER: 0,
+        Exp.FEAR: 1,
+        Exp.HAPPINESS: 2,
+        Exp.NEUTRAL: 3,
+        Exp.SADNESS: 4,
+        Exp.SURPRISE: 5
+    }
+STANDARD_COUNT = 20
+
+# - Datasets ------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+print(' LOADING CSV DATAS                                            ')
+DATAS = FER2013Dataset()
+DATAS.load_csv(fer2013_path)
+print(' -----                                                   DONE ')
+
+DATAS.shuffle()
+
+# Get some standard pics to compare with.
+# standard_pics = {
+#         Exp.ANGER: DATAS.get_n_pictures([Exp.ANGER], count=STANDARD_COUNT),
+#         Exp.FEAR: DATAS.get_n_pictures([Exp.FEAR], count=STANDARD_COUNT),
+#         Exp.HAPPINESS: DATAS.get_n_pictures([Exp.HAPPINESS], count=STANDARD_COUNT),
+#         Exp.NEUTRAL: DATAS.get_n_pictures([Exp.NEUTRAL], count=STANDARD_COUNT),
+#         Exp.SADNESS: DATAS.get_n_pictures([Exp.SADNESS], count=STANDARD_COUNT),
+#         Exp.SURPRISE: DATAS.get_n_pictures([Exp.SURPRISE], count=STANDARD_COUNT),
+#     }
+
+standard_pics = DATAS.get_n_pictures([Exp.HAPPINESS], count=STANDARD_COUNT)
+
+# training_pics = DATAS.get_n_pictures([Exp.ANGER, Exp.FEAR, Exp.HAPPINESS,
+#                                       Exp.NEUTRAL, Exp.SADNESS, Exp.SURPRISE],
+#                                      count=50, start=10)
+
+# training_pics = DATAS.get_n_pictures([Exp.HAPPINESS],
+#                                      count=200, start=0)
+
+validation_pics = DATAS.get_n_pictures([Exp.HAPPINESS, Exp.FEAR, Exp.ANGER,
+                                       Exp.NEUTRAL, Exp.SADNESS, Exp.SURPRISE],
+                                       target="validation",
+                                       count=20, start=10)
+
+print(len(validation_pics))
+
+# - Models --------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+m_ha = load_model(model_path)
+
+# - Get Prediction ------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+PREDICT_TUPLES = []
+
+# Init predict tuples with first expression class
+# model, exp = models[0]
+model, exp = (m_ha, Exp.HAPPINESS)
+X1 = []
+X2 = []
+print("Predict for : "+exp.to_str())
+for j in range(0, len(validation_pics)):
+    sum = 0
+    for k in range(0, STANDARD_COUNT):
+        img, exp = standard_pics[k]
+        X1.append(img)
+    for k in range(0, STANDARD_COUNT):
+        img, exp = validation_pics[j]
+        X2.append(img)
+
+X1 = numpy.asarray(X1)
+X2 = numpy.asarray(X2)
+sums = model.predict([X1, X2], verbose=1)
+
+print(sums)
+
+sums = sums.reshape(STANDARD_COUNT*len(validation_pics))
+sums = sums.tolist()
+
+i = 0
+while i < len(sums):
+    img, exp = validation_pics[int(i/STANDARD_COUNT)]
+    s = exp.to_str()
+    for j in range(0, STANDARD_COUNT):
+        exp1 = "D"
+        if sums[i] > 0.5:
+            exp1 = "S"
+        s += " " + exp1
+        i += 1
+    print(s)
